67e95dafeb31d30acd5d67f766a4f480f94d4e49
Joel K George <joelk9895@gmail.com>
2025-07-12 17:56:42 +0530

    Added vercel support

--- src/main.ts
+++ src/main.ts
@@ -1,37 +1,39 @@
+// src/main.ts
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
-// Load environment variables from .env file
 dotenv.config();
 
-async function bootstrap() {
+export async function createNestServer() {
   const app = await NestFactory.create(AppModule);
+
   app.enableCors({
-    origin: process.env.CORS_ORIGIN || '*', // Allow all origins by default, or specify a specific origin
+    origin: process.env.CORS_ORIGIN || '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
 
-  // Use DocumentBuilder to create the Swagger configuration
   const config = new DocumentBuilder()
     .setTitle('Jobloo API')
     .setDescription('API documentation for Jobloo application')
     .setVersion('1.0')
-    .addBearerAuth() // This is important for testing protected endpoints
+    .addBearerAuth()
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  const port = process.env.PORT ?? 3000;
-  console.log(`Server running on port ${port}`);
-  await app.listen(port);
-}
-export async function createNestServer() {
-  const app = await NestFactory.create(AppModule);
   return app;
 }
 
-void bootstrap();
+// Local run only
+if (process.env.NODE_ENV !== 'production') {
+  createNestServer().then((app) => {
+    const port = process.env.PORT || 3000;
+    app.listen(port).then(() => {
+      console.log(`🚀 Local server running on http://localhost:${port}`);
+    });
+  });
+}
